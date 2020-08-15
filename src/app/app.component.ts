9c54002b1fe0b9bcb52a59a3e8a1b0a63b8be366
keiny rivas <keinyrivas@gmail.com>
2020-08-15 19:57:19 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { PersonaService } from './services/personas.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicio2';
+  persona: Array<any>;
+
+  constructor(private personaService: PersonaService) {
+    this.persona = new Array<any>();
+  }
+
+  getAllPersona() {
+    this.personaService.getPersona().subscribe((persona) => {
+      this.persona = persona;
+    });
+  }
 }
